50e4fed78768465cd1a3bac7498a1e73c8b7e7d6
PalindromicBreadLoaf <palindromicbreadloaf@tuta.com>
2026-07-13 11:17:09 -0400

    Create dummy files for subsystems

---
+++ include/ryefs.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright(c) 2026: PalindromicBreadLoaf (palindromicbreadloaf@tuta.com)
+ *
+ * Please see the included LICENSE file for more information.
+ */
+
+// Master public include for shared library users.
+
+#ifndef RYEFS_RYEFS_H
+#define RYEFS_RYEFS_H
+
+#include "fs_constants.h"
+#include "fs_structs.h"
+#include "fs_journal.h"
+
+#endif //RYEFS_RYEFS_H
